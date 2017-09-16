b5096c1326ee35e94dedc30fa17603b627d98ded
Simon Jespersen <simjes91@me.com>
2017-09-16 13:55:36 +0200

    config ok and consistent with vue plugin for vs2017

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -4,13 +4,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './components/home/home.vue';
 import Counter from './components/counter/counter.vue';
+import FetchData from './components/fetchdata/fetchdata.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
 	{ path: '/', component: Home },
 	{ path: '/counter', component: Counter },
-	{ path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue') }
+	{ path: '/fetchdata', component: FetchData }
 ];
 
 new Vue({
