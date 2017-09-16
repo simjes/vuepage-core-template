3d06dfa737b1af8943046b9936892c53770f13e7
Simon Jespersen <simjes91@me.com>
2017-09-16 13:37:05 +0200

    vue custom setup

---
+++ ClientApp/boot.ts
@@ -0,0 +1,20 @@
+import './css/site.css';
+import 'bootstrap';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './components/home/home.vue';
+import Counter from './components/counter/counter.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{ path: '/', component: Home },
+	{ path: '/counter', component: Counter },
+	{ path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue') }
+];
+
+new Vue({
+	el: '#app-root',
+	router: new VueRouter({ mode: 'history', routes: routes }),
+	render: h => h(require('./components/app/app.vue'))
+});
