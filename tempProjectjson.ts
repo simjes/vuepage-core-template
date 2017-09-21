f7210e508d4c1857723d64b0c22eb283cfcdde32
Simon Jespersen <simjes91@me.com>
2017-09-21 21:40:10 +0200

    temp data

---
+++ tempProjectjson.ts
@@ -0,0 +1,483 @@
+﻿var temp = {
+	"objects": [
+			{
+				"_id": "5872297e555e135e35000178",
+				"slug": "react-material",
+				"title": "React Material",
+				"content": "<p>Portfolio page in React.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-08T11:58:54.068Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-08T11:58:54.068Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:40:58.744Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "React, Mui CSS",
+					"image": "http://i.imgur.com/NHOwOda.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/ReactWeb"
+						},
+						{
+							"linkto": "Webpage",
+							"url": "https://simjes.github.io/ReactWeb/"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/NHOwOdal.png",
+					"description": "Portfolio page in React. Using Mui CSS for material design."
+				}
+			},
+			{
+				"_id": "58713ee8172deea821b62471",
+				"slug": "angular-webpage",
+				"title": "Angular Webpage",
+				"content": "<p>Portfolio webpage made with Angular and Angular Material.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:17:59.996Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:17:59.996Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:41:14.475Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Angular",
+					"image": "http://i.imgur.com/j2rCiCk.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/ngMaterial-website"
+						},
+						{
+							"linkto": "Webpage",
+							"url": "http://simjes.github.io/ngMaterial-website"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/j2rCiCkl.png",
+					"description": "Portfolio webpage made with Angular and Angular Material."
+				}
+			},
+			{
+				"_id": "58713f22ee1a48312348fc53",
+				"slug": "geek-events-app",
+				"title": "Geek Events App",
+				"content": "<p>Cross-platform application for browsing events. The user can browse events by categories and by local events. Uses Facebook login for adding new events.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:18:58.016Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:18:58.016Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:15:30.758Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Ionic, Facebook Graph API",
+					"image": "http://i.imgur.com/6agLPiz.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/gevents_app"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/6agLPizl.png",
+					"description": "Cross-platform application for browsing events. The user can browse events by categories and by local events. Uses Facebook login for adding new events."
+				}
+			},
+			{
+				"_id": "58713f3fee1a48312348fc57",
+				"slug": "event-server",
+				"title": "Event Server",
+				"content": "<p>RESTful server for storing events. Saves the data in MongoDB.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:19:27.817Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:19:27.817Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:41:43.390Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Node, MongoDB",
+					"image": "http://i.imgur.com/Uo3QWry.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/gevents_server"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/Uo3QWryl.png",
+					"description": "RESTful server for storing events. Saves the data in MongoDB."
+				}
+			},
+			{
+				"_id": "587142276159571d21e33cf5",
+				"slug": "snagpaper",
+				"title": "Snagpaper",
+				"content": "<p>Uses Imgur API to find images with the wallpaper tag. Save and favorite images.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:31:50.954Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:31:50.955Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:51:36.416Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Android, Realm, Imgur API",
+					"image": "http://i.imgur.com/vfQgIO9.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/Snagpaper"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/vfQgIO9l.png",
+					"description": "Uses Imgur API to find images with the wallpaper tag. Save and favorite images."
+				}
+			},
+			{
+				"_id": "58714168172deea821b624b6",
+				"slug": "watchpaper",
+				"title": "WatchPaper",
+				"content": "<p>Swift app for changing wallpaper based on hour of the day. Runs with launchd. Includes KLWP for Android.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:28:40.560Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:28:40.560Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:42:02.256Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Swift, SQLite",
+					"image": "http://i.imgur.com/H73I7Bs.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/WatchPaper"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/H73I7Bsl.png",
+					"description": "Swift app for changing wallpaper based on hour of the day. Runs with launchd. Includes KLWP for Android."
+				}
+			},
+			{
+				"_id": "58713e37ee1a48312348fc27",
+				"slug": "shoplistswift",
+				"title": "ShopListSwift",
+				"content": "<p>iOS application for writing down notes and groceries. Stores the data in a Realm database.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:15:03.081Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:15:03.081Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:13:24.858Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Swift, iOS, Realm",
+					"image": "http://i.imgur.com/RWhLAih.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/ShopListSwift"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/RWhLAihl.png",
+					"description": "iOS application for writing down notes and groceries. Stores the data in a Realm database."
+				}
+			},
+			{
+				"_id": "5871408eee1a48312348fc7d",
+				"slug": "firestorm",
+				"title": "Firestorm",
+				"content": "<p>A remote control for a gas fireplace. The Raspberry Pi runs a Python or a Golang HTTP server to listen for commands from a Android application. Based on the request it receives, the Raspberry Pi turns the heat up or down.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:25:02.585Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:25:02.585Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:43:23.452Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Android, Raspberry PI, Go, Python",
+					"image": "http://i.imgur.com/zepUUwe.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/Firestorm"
+						},
+						{
+							"linkto": "Youtube",
+							"url": "https://www.youtube.com/watch?v=aHwYtt6lNsw"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/zepUUwel.png",
+					"description": "A remote control for a gas fireplace. The Raspberry Pi runs a Python or a Golang HTTP server to listen for commands from a Android application. Based on the request it receives, the Raspberry Pi turns the heat up or down."
+				}
+			},
+			{
+				"_id": "58713e86ee1a48312348fc39",
+				"slug": "dhis2-tools",
+				"title": "DHIS2 tools",
+				"content": "<p>Updating the dhis2-tools Ubuntu package. dhis2-tools is a collection of tools and utilities for installing and managing DHIS2 applications on an Ubuntu server.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:16:22.476Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:16:22.476Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:43:37.250Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Ubuntu Package",
+					"image": "http://i.imgur.com/tMO7JQW.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/dhis2-tools"
+						},
+						{
+							"linkto": "Launchpad",
+							"url": "https://launchpad.net/~simjes91/+archive/ubuntu/dhis2-tools"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/tMO7JQWl.png",
+					"description": "Updating the dhis2-tools Ubuntu package. dhis2-tools is a collection of tools and utilities for installing and managing DHIS2 applications on an Ubuntu server."
+				}
+			},
+			{
+				"_id": "58713df9ee1a48312348fc21",
+				"slug": "weatherfuse",
+				"title": "WeatherFuse",
+				"content": "<p>Cross-platform app to check the weather based on your GPS location, uses data from OpenWeatherMap.org. Testing out Fuse for building native cross-platform apps. New update broke the build, so stopped working on it until it is more stable.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:14:01.454Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:14:01.454Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:15:06.256Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Fuse, OpenWeatherMap API",
+					"image": "http://i.imgur.com/eje0u52.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/WeatherFuse"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/eje0u52l.png",
+					"description": "Cross-platform app to check the weather based on your GPS location, uses data from OpenWeatherMap.org. Testing out Fuse for building native cross-platform apps. New update broke the build, so stopped working on it until it is more stable."
+				}
+			},
+			{
+				"_id": "58713fa0172deea821b6247c",
+				"slug": "organisation-unit-manager",
+				"title": "Organisation Unit Manager",
+				"content": "<p>WebApp for DHIS2. Can be used to browse, edit and add organisational units in DHIS2.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:21:04.544Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:21:04.544Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:23:08.903Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "DHIS2 WebApp, Angular, DHIS2 API",
+					"image": "http://i.imgur.com/4kUsQAS.jpg",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/dhis2-orgunit"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/4kUsQASl.jpg",
+					"description": "WebApp for DHIS2. Can be used to browse, edit and add organisational units in DHIS2."
+				}
+			},
+			{
+				"_id": "58714021ee1a48312348fc79",
+				"slug": "simcv",
+				"title": "SimCV",
+				"content": "<p>Uses OpenCV library on Android for face tracking, circle detection, color tracking and more. This was made as part of my bachelor thesis.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:23:13.456Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:23:13.456Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:22:45.024Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Android, OpenCV",
+					"image": "http://i.imgur.com/YWWMolv.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/OpenCV-Bachelor"
+						},
+						{
+							"linkto": "Youtube",
+							"url": "https://www.youtube.com/watch?v=oCViAhgrZn0"
+						},
+						{
+							"linkto": "Android",
+							"url": "https://play.google.com/store/apps/details?id=com.simjessimsol.simcv&hl=en"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/YWWMolvl.png",
+					"description": "Uses OpenCV library on Android for face tracking, circle detection, color tracking and more. This was made as part of my bachelor thesis."
+				}
+			},
+			{
+				"_id": "58714066172deea821b62486",
+				"slug": "simcv-game",
+				"title": "SimCV Game",
+				"content": "<p>A game made with LibGDX and OpenCV. It uses the Android camera to track a specified color, when the user moves the color the ingame character moves.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:24:22.703Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:24:22.703Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:44:37.962Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Android, OpenCV, LibGDX",
+					"image": "http://i.imgur.com/fUkm808.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/OpenCV-Bachelor"
+						},
+						{
+							"linkto": "Youtube",
+							"url": "https://www.youtube.com/watch?v=OY1HrQPOHJY"
+						},
+						{
+							"linkto": "Android",
+							"url": "https://play.google.com/store/apps/details?id=com.simjessimsol.simcvgame.android&hl=en"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/fUkm808l.png",
+					"description": "A game made with LibGDX and OpenCV. It uses the Android camera to track a specified color, when the user moves the color the ingame character moves."
+				}
+			},
+			{
+				"_id": "58713fc8ee1a48312348fc72",
+				"slug": "streemer",
+				"title": "Streemer",
+				"content": "<p>Cross-platform application for watching streams from different streaming website. Twitch API has changed, so it is currently broken.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:21:44.291Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:21:44.291Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:16:09.947Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Ionic, Twitch API, Hitbox API",
+					"image": "http://i.imgur.com/NS3eG7U.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/Streemer"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/NS3eG7Ul.png",
+					"description": "Cross-platform application for watching streams from different streaming website. Twitch API has changed, so it is currently broken."
+				}
+			},
+			{
+				"_id": "58713eb5ee1a48312348fc3f",
+				"slug": "shoplist",
+				"title": "ShopList",
+				"content": "<p>Shopping list application for Android.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:17:09.008Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:17:09.008Z",
+				"status": "published",
+				"modified_at": "2017-01-08T21:13:58.392Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Android",
+					"image": "http://i.imgur.com/NSlX1ci.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/ShopList"
+						},
+						{
+							"linkto": "Android",
+							"url": "https://play.google.com/store/apps/details?id=com.simjes.shoplist"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/NSlX1cil.png",
+					"description": "Shopping list application for Android."
+				}
+			},
+			{
+				"_id": "58713f61ee1a48312348fc5a",
+				"slug": "picturesort",
+				"title": "pictureSort",
+				"content": "<p>Makes a copy of all the unique pictures in the Windows picture folder and sorts them in the a new folder. In the new folder the pictures are put in folders sorted by date of when the pictures was taken.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:20:01.303Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:20:01.303Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:45:11.681Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "C#",
+					"image": "http://i.imgur.com/zk2JhI5.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/simjes/pictureSort"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/zk2JhI5l.png",
+					"description": "Makes a copy of all the unique pictures in the Windows picture folder and sorts them in the a new folder. In the new folder the pictures are put in folders sorted by date of when the pictures was taken."
+				}
+			},
+			{
+				"_id": "587140b7172deea821b6249a",
+				"slug": "photo-viewer",
+				"title": "Photo Viewer",
+				"content": "<p>A desktop application that can be used to view and upload pictures to a server, as well as edit metadata of the pictures. The pictures can be filtered by metadata and by the folders they are located in. The server uses a SQLite database to keep track of all the information.</p>",
+				"bucket": "58712288172deea821b623eb",
+				"type_slug": "projectdetails",
+				"created_at": "2017-01-07T19:25:43.771Z",
+				"created_by": "5871222950a26cd7211ddf14",
+				"created": "2017-01-07T19:25:43.771Z",
+				"status": "published",
+				"modified_at": "2017-01-08T18:45:25.467Z",
+				"modified_by": "5871222950a26cd7211ddf14",
+				"metadata": {
+					"tags": "Java, Hibernate, SQLite, Client, Server",
+					"image": "http://i.imgur.com/nWftgII.png",
+					"links": [
+						{
+							"linkto": "Github",
+							"url": "https://github.com/snorheim/DAT210G"
+						}
+					],
+					"thumbnail": "http://i.imgur.com/nWftgIIl.png",
+					"description": "A desktop application that can be used to view and upload pictures to a server, as well as edit metadata of the pictures. The pictures can be filtered by metadata and by the folders they are located in. The server uses a SQLite database to keep track of all the information."
+				}
+			}
+		],
+		"total": 17
+}
